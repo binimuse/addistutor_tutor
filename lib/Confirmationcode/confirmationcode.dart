dda1fea176129ca3aaa42e0e730439f04ca1680f
binimuse <46643391+binimuse@users.noreply.github.com>
2022-01-05 15:43:22 +0300

    tgt

--- lib/Confirmationcode/confirmationcode.dart
+++ lib/Confirmationcode/confirmationcode.dart
@@ -1,7 +1,11 @@
 import 'dart:io';
 
+import 'package:addistutor_tutor/Home/components/homescreen.dart';
 import 'package:addistutor_tutor/Profile/app_theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 import '../constants.dart';
 
@@ -13,19 +17,27 @@ class CodeScreen extends StatefulWidget {
 }
 
 final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-// Barcode? result;
-// QRViewController? controller;
+Barcode? result;
+QRViewController? controller;
 
 class _FeedbackScreenState extends State<CodeScreen> {
-  // @override
-  // void reassemble() {
-  //   super.reassemble();
-  //   if (Platform.isAndroid) {
-  //     controller!.pauseCamera();
-  //   } else if (Platform.isIOS) {
-  //     controller!.resumeCamera();
-  //   }
-  // }
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  void initState() {
+    if (result != null) {
+      result = null;
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,64 +65,43 @@ class _FeedbackScreenState extends State<CodeScreen> {
               height: MediaQuery.of(context).size.height,
               child: Column(
                 children: <Widget>[
-                  // Expanded(
-                  //   flex: 5,
-                  //   child: QRView(
-                  //     key: qrKey,
-                  //     onQRViewCreated: _onQRViewCreated,
-                  //   ),
-                  // ),
-                  Container(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: const Text(
-                      'Enter  Code here',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  Expanded(
+                    flex: 2,
+                    child: QRView(
+                      key: qrKey,
+                      onQRViewCreated: _onQRViewCreated,
                     ),
                   ),
-                  _buildComposer(),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Center(
-                      child: Container(
-                        width: 120,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(4.0)),
-                          boxShadow: <BoxShadow>[
-                            BoxShadow(
-                                color: Colors.grey.withOpacity(0.6),
-                                offset: const Offset(4, 4),
-                                blurRadius: 8.0),
-                          ],
-                        ),
-                        child: Material(
-                          color: kPrimaryColor,
-                          child: InkWell(
-                            onTap: () {
-                              FocusScope.of(context).requestFocus(FocusNode());
-                            },
-                            child: Center(
-                              child: Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Text(
-                                  'Send',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.white,
+                  Expanded(
+                      flex: 1,
+                      child: Center(
+                        child: (result != null)
+                            ? Column(children: [
+                                Text(
+                                    'Sucessfully Scaned Qr code: ${result!.code}'),
+                                Material(
+                                  color: kPrimaryColor,
+                                  child: InkWell(
+                                    onTap: () {
+                                      snackBar();
+                                    },
+                                    child: Center(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(24.0),
+                                        child: Text(
+                                          'Confirm',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  )
+                              ])
+                            : Text('Canot find Qr Code'),
+                      )),
                 ],
               ),
             ),
@@ -120,61 +111,29 @@ class _FeedbackScreenState extends State<CodeScreen> {
     );
   }
 
-  Widget _buildComposer() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16, left: 32, right: 32),
-      child: Container(
-        decoration: BoxDecoration(
-          color: AppTheme.white,
-          borderRadius: BorderRadius.circular(8),
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.grey.withOpacity(0.8),
-                offset: const Offset(4, 4),
-                blurRadius: 8),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(25),
-          child: Container(
-            padding: const EdgeInsets.all(4.0),
-            constraints: const BoxConstraints(minHeight: 80, maxHeight: 160),
-            color: AppTheme.white,
-            child: SingleChildScrollView(
-              padding:
-                  const EdgeInsets.only(left: 10, right: 10, top: 0, bottom: 0),
-              child: TextField(
-                maxLines: null,
-                onChanged: (String txt) {},
-                style: TextStyle(
-                  fontFamily: AppTheme.fontName,
-                  fontSize: 16,
-                  color: AppTheme.dark_grey,
-                ),
-                cursorColor: Colors.blue,
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Enter your feedback...'),
-              ),
-            ),
-          ),
-        ),
+  snackBar() {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Sucessfully  Scaned Qr code"),
+        duration: const Duration(seconds: 1),
       ),
     );
   }
 
-  // void _onQRViewCreated(QRViewController controller) {
-  //   controller = controller;
-  //   controller.scannedDataStream.listen((scanData) {
-  //     setState(() {
-  //       result = scanData;
-  //     });
-  //   });
-  // }
+  void _onQRViewCreated(QRViewController controller) async {
+    controller = controller;
 
-  // @override
-  // void dispose() {
-  //   controller?.dispose();
-  //   super.dispose();
-  // }
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+
+    super.dispose();
+  }
 }
